69ac1932d6427deafa2ab54747636e873d3e49b8
efss7 <ericfelipess07@gmail.com>
2022-12-15 17:37:34 -0300

    Componente Tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
 import Layout from "../components/Layout";
+import Table from "../components/Table";
+import Client from "../core/Client";
 
 export default function Home() {
+  const ClientMock = [
+    new Client('Eric', 20, '1'),
+    new Client('Felipe', 20, '2'),
+    new Client('Ueslei', 24, '3'),
+    new Client('Igor', 8, '4'),
+  ]
+
+  function clientSelected(client: Client) {
+    console.log(client.name);
+  }
+  function clientDeleted(client: Client) {
+    console.log(client.name);
+  }
   return (
     <div className="flex justify-center items-center h-screen
     bg-gradient-to-r from-blue-500 to-purple-500
     text-white
     ">
       <Layout title="Cadastro Simples">
-        <span>Conteúdo</span>
+        <Table clients={ClientMock}
+          clientSelected={clientSelected}
+          clientDeleted={clientDeleted} />
       </Layout>
     </div>
   )
