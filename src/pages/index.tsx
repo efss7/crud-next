50b302b1ca5fd4e2156ce46a61e59845fc59205a
efss7 <ericfelipess07@gmail.com>
2022-12-15 18:50:10 -0300

    Componente Botão

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button";
 import Layout from "../components/Layout";
 import Table from "../components/Table";
 import Client from "../core/Client";
@@ -22,6 +23,9 @@ export default function Home() {
     text-white
     ">
       <Layout title="Cadastro Simples">
+        <div className="flex justify-end ">
+          <Button color="green" className="mb-4" >Novo Cliente</Button>
+        </div>
         <Table clients={ClientMock}
           clientSelected={clientSelected}
           clientDeleted={clientDeleted} />
